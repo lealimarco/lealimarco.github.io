326b0637369cca5921e364e671554ad18900224b
lealimarco <lealimarco@hotmail.it>
2026-07-03 11:35:48 +0200

    #2

--- js/script.js
+++ js/script.js
@@ -1,1 +1,2 @@
-// future enhancements
+function toggleTheme(){document.body.classList.toggle('dark');localStorage.theme=document.body.classList.contains('dark')?'dark':'light';}
+window.onload=()=>{if(localStorage.theme==='dark')document.body.classList.add('dark');};
